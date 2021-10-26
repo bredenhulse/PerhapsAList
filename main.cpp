ddc7c1fcd42d3735e09ffafea5e75c3aab56ea0c
bredenhulse <bredenhulse@gmail.com>
2021-10-26 08:35:49 -0500

    yes

--- main.cpp
+++ main.cpp
@@ -10,7 +10,24 @@ Description: It is a linked list.
 using namespace std;
 
 int main(){
+    List beiju;
     ifstream infile;
-    infile open("Test.txt");
-    list beiju();
+    infile.open("Test.txt");
+    ofstream fout;
+    fout.open("outfile");
+    string testCase;
+    while(infile >> testCase){
+        for(i = 0; i < testCase.length; i++){
+            if(testCase[i] = '['){
+               beiju.begin(); 
+            }
+            else if(testCase[i] = ']'){
+                
+            }
+            else{
+
+            }
+        }
+    }
+    infile.close();
 }
